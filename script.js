be97db6668c60011aa7c0eebab0ab00d47459f0b
Barb Aponi Tcruz (@barb.dev) <143009855+babbiaponi@users.noreply.github.com>
2023-11-06 19:32:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function imprimirMensagem() {
-    console.log("Conexão feita com sucesso!");
-}
+// Método Simples - Usando innerHTML
+document.getElementById('titulo').innerHTML = 'Produtos à Venda';
 
-for (let i = 0; i < 3; i++) {
-    imprimirMensagem();
-}
+// Método Complexo - Criando elementos programaticamente
+const produtoContainer = document.getElementById('produto-container');
+
+const produto = document.createElement('div');
+produto.className = 'produto';
+
+const nome = document.createElement('h2');
+nome.innerHTML = 'Creme Hidratante';
+
+const descricao = document.createElement('p');
+descricao.innerHTML = 'Para peles oleosa';
+
+const preco = document.createElement('span');
+preco.innerHTML = 'Preço: R$ 99.99';
+
+const imagem = document.createElement('img');
+imagem.src = 'img/creme pele sensiveis.jpeg';
+imagem.alt = 'Imagem do Produto A';
+
+produto.appendChild(nome);
+produto.appendChild(descricao);
+produto.appendChild(preco);
+produto.appendChild(imagem);
+
+produtoContainer.appendChild(produto);
